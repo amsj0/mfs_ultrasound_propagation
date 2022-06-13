e0e7a2dddacb186611a7ee4877edf3ac65f24e39
ageusp <agesinaldo.silva@usp.br>
2022-06-13 19:36:01 -0300

    compliant with area parameter

--- octave/fn_analyze.m
+++ octave/fn_analyze.m
@@ -10,8 +10,8 @@ function rd = fn_analyze(path,datafile,configfile,analysisfile)
 	ndx.T = fn_integrate_indexing(ppt_per_surface,length(b.Ti.x),length(b.To.x));
 	ndx.R = fn_integrate_indexing(ppt_per_surface,length(b.Ri.x),length(b.Ro.x));
 
-	response.pitch.size = (length(T.a{:})-ppt_per_surface+1);
-	response.catch.size = (length(R.a{:})-ppt_per_surface+1);
+	response.pitch.size = (length(T.a)-ppt_per_surface+1);
+	response.catch.size = (length(R.a)-ppt_per_surface+1);
 
 	field.range.ptt = zeros(response.pitch.size,length(b.D.c));
 
